a2925a16d25e26e1e1fcec29ff2b7ad7256bfd9c
Ana Laura <analaurass45@gmail.com>
2023-03-05 17:11:25 -0300

    adicionando o cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { router } from "./routes";
+import cors from "cors"
 
 const app = express();
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use(router);
 
-app.listen(3030, () => console.log("Server listening on port 3030"))
+app.listen(3030, () => 
+    console.log("Server listening on port 3030")
+);
