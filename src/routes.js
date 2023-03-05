ac4cd7d9643012d5ef54fb07e9adfdc70d629173
Ana Laura <analaurass45@gmail.com>
2023-03-05 17:24:43 -0300

    adicionando as rotas criadas

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
 import {Router} from 'express';
 
 import UserController from './controller/UserController';
+import  {AuthController}  from './controller/AuthController';
+import { AuthMiddleware } from './middlewares/auth';
 
 const router = Router();
+const auth = new AuthController();
 
+router.post("/login", auth.authenticate)
+router.get("/home", UserController.findUsers)
 router.post('/register', UserController.createUser)
-router.post('/login', UserController.Login)
+router.get("/users",  UserController.findUsers)
+router.put("/update/:id", AuthMiddleware, UserController.updateUser)
+router.delete("/delete/:id", UserController.deleteUser)
+
 
 export {router};
