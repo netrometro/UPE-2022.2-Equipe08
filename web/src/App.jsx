e2e9f7d1e8fa18f23470b779534a98ada104d248
Ana Laura <analaurass45@gmail.com>
2023-03-11 01:20:23 -0300

    modificando a home e adicionando as notas

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,30 +1,19 @@
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import First from './pages/first/first.jsx'
 import RegisterForm from './pages/cadastro/cadastro.jsx'
 import LoginForm from './pages/Login/login.jsx'
 import { Home } from './pages/home/home.jsx'
 import { AuthProvider } from './context/auth.jsx'
 import { PrivateRoute } from './routes/privateRoutes.jsx'
+import Navbar from './components/navbar.jsx'
+// import Archived from './pages/arquivados/archived.jsx'
+
 export const App = () => {
   return (
     <AuthProvider>
-
       <Router> 
-        <header>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </header>
+        <Navbar/>
         <Routes>
-
           <Route exact path="/" element={<First/>}>
           </Route>
           <Route path="/login" element={<LoginForm/>}>
@@ -33,8 +22,7 @@ export const App = () => {
           </Route>
           <Route path="/home" element={<PrivateRoute/>}>
             <Route path='/home' element={<Home/>}></Route>
-          </Route>
-         
+          </Route>        
         </Routes>
       </Router>
     </AuthProvider>
