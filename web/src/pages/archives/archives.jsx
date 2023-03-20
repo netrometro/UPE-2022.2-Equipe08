a104cc34ba8f28c2f7cbfb7381c50b4785a881a9
João Lyra <joaolyra24@gmail.com>
2023-03-19 21:12:35 -0300

    pagina de arquivados
    criação da pagina de arquivados, com as rotas e os geradores de lista

--- web/src/pages/archives/archives.jsx
+++ web/src/pages/archives/archives.jsx
@@ -1,16 +1,17 @@
 import { AuthContext } from "../../context/auth";
 import { useContext } from "react";
-import './home.css'
-import { AiOutlineInbox} from "react-icons/ai";
+import './archives.css'
+import { BiHomeAlt2} from "react-icons/bi";
 import { RiDeleteBin4Line } from "react-icons/ri"
 
-import NotesList from "../../components/notesList";
+
 import { useState } from "react";
 import { api } from "../../services/api";
 
 import {Link} from "react-router-dom"
+import NotesArchList from "../../components/notesArchList";
 
-export const Home = () =>{
+export const Archives = () =>{
 
     const session = JSON.parse(localStorage.getItem("@Auth:user"))
     
@@ -50,24 +51,22 @@ export const Home = () =>{
     return (
         <section className="home">
             <header>
-                <h1>Welcome to Notes-App</h1>
+                <h1>Archived Notes</h1>
                 <aside>
                     <RiDeleteBin4Line className="trash-icon" size="2.3em" ></RiDeleteBin4Line>
-                    <Link to="/archive">
-                        <AiOutlineInbox className="archive-icon" size="2.5em"></AiOutlineInbox>
+                    <Link to="/home">
+                        <BiHomeAlt2 className="home-icon" size="2.5em"></BiHomeAlt2>
                     </Link>
-                    <button className="home-button" onClick={retrieve}>Show notes</button>
-
-                    <button className="home-button"onClick={handleSignOut}>Sign out</button>
                     
+                    <button className="home-button" onClick={retrieve}>Show notes</button>
+                    <button className="home-button"onClick={handleSignOut}>Sign out</button>
                 </aside>
             </header>
             <div>
-               <NotesList notes={notes} handleAddNote={addNote}/>
+               <NotesArchList notes={notes} handleAddNote={addNote}/>
             </div> 
 
 
         </section>
     );
 }
-
