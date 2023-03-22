e39723d824f3f9f01512e5da7223ef355986de8f
Ewerton Mendonça <netrometro@yahoo.com.br>
2023-03-22 14:40:40 -0300

    Configuração do Netlify

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -6,7 +6,7 @@ import { App } from "./App.jsx";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>OK</h1>
   </React.StrictMode>
 );
 
