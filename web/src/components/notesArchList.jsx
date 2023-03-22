efabedec8ea4580700f82e926aacad893792cbc4
Ewerton Mendonça <netrometro@yahoo.com.br>
2023-03-22 15:04:20 -0300

    Configuração do Netlify

--- web/src/components/notesArchList.jsx
+++ web/src/components/notesArchList.jsx
@@ -1,19 +1,19 @@
 import Notes from "./notes";
 import "./note.css"
-import AddNote from "./AddNote";
+//import AddNote from "./AddNote";
 
 
-function NotesArchList({notes, handleAddNote}){
+function NotesArchList({ notes, handleAddNote }) {
     const archivedNotes = notes.filter(note => note.isArchived);
 
-        return (
-            <div className="notes-list">
-                {archivedNotes.map((note)=>(
-                    <Notes id={note.id} text={note.text} isArchived={note.isArchived}/>
-                ))}
-                
-            </div>
-        )
-    }
+    return (
+        <div className="notes-list">
+            {archivedNotes.map((note) => (
+                <Notes id={note.id} text={note.text} isArchived={note.isArchived} />
+            ))}
+
+        </div>
+    )
+}
 
 export default NotesArchList;
