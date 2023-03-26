9a5325dde79a3ae33aec1d6d38b8b092b95b45fd
João Lyra <joaolyra24@gmail.com>
2023-03-26 20:57:36 -0300

    Rotas da lixeira
    feitas as rotas da lixeira

--- server/src/routes.js
+++ server/src/routes.js
@@ -20,6 +20,8 @@ router.get("/notes/user/:id", notesController.getNotes)
 router.delete("/notes/delete/:id", notesController.deleteNotes)
 router.put("/notes/archive/:id", AuthMiddleware, notesController.archiveNote);
 router.put("/notes/unarchive/:id", AuthMiddleware, notesController.unarchiveNote);
+router.put("/notes/trash/:id", AuthMiddleware, notesController.trashNotes);
+router.put("/notes/recover/:id", AuthMiddleware, notesController.recoverNote);
 
 
 
