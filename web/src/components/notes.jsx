d114ee144b1503aa5f76341072d05cc679408843
João Lyra <joaolyra24@gmail.com>
2023-03-26 20:59:34 -0300

    alterado as funções das notas padrão
    mudança nas notas padrão

--- web/src/components/notes.jsx
+++ web/src/components/notes.jsx
@@ -1,40 +1,28 @@
 import { useState } from "react";
 import { api } from "../services/api";
-import {RiDeleteBin4Line } from "react-icons/ri";
-import { BiArchiveIn, BiArchiveOut} from "react-icons/bi";
+import {TbTrash } from "react-icons/tb";
+import { BiArchiveIn} from "react-icons/bi";
 import "./note.css";
 
 function Notes({id, text, archived, setNotes}){
-    const [isArchived, setIsArchived] = useState(archived);
+    const [isArchived] = useState(archived);
 
     const handleArchiveNote = async () => {
         try {
             const response = await api.put(`/notes/archive/${id}`);
             if (response.status === 200) {
-                setIsArchived(true);
-                setNotes(prevNotes => prevNotes.filter(note => note.id !== id));
+                alert ("Nota arquivada com sucesso");
             }
         } catch (error) {
             console.error(error);
         }
     }
 
-    const handleUnarchiveNote = async () => {
+    const handleMoveToTrash = async() => {
         try {
-            const response = await api.put(`/notes/unarchive/${id}`);
-            if (response.status === 200) {
-                setIsArchived(false);
-                setNotes(prevNotes => prevNotes.filter(note => note.id !== id));
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    }
-    const handleDeleteForever = async() => {
-        try {
-            const response = await api.delete(`/notes/delete/${id}`);
+            const response = await api.put(`/notes/trash/${id}`);
             if (response.status === 200){
-                alert ("Nota deletada com sucesso")
+                alert ("Nota movida para a lixeira")
             }
         } catch(error){
             console.log(error)
@@ -45,9 +33,8 @@ function Notes({id, text, archived, setNotes}){
         <div className={`note ${isArchived ? "archived" : ""}`}>
             <span>{text}</span>
             <footer className="note-footer">
-                <RiDeleteBin4Line className="trash-note" onClick={handleDeleteForever}></RiDeleteBin4Line>
-                {!isArchived && <BiArchiveIn className="archive-note" onClick={handleArchiveNote}></BiArchiveIn>}
-                {!isArchived && <BiArchiveOut className="unarchive-note" onClick={handleUnarchiveNote}></BiArchiveOut>}
+                <TbTrash className="trash-note" onClick={handleMoveToTrash}></TbTrash>
+                <BiArchiveIn className="archive-note" onClick={handleArchiveNote}></BiArchiveIn>
             </footer>
         </div>
     )
