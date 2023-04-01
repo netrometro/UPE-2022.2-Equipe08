3d67cfbe791932cd2c5acf02d7fc20b8e9dfff4f
João Lyra <joaolyra24@gmail.com>
2023-03-31 21:03:37 -0300

    botão para fixar
    criado o botão para fixar a nota

--- web/src/components/notes.jsx
+++ web/src/components/notes.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { api } from "../services/api";
-import {TbTrash } from "react-icons/tb";
+import {TbTrash, TbPin } from "react-icons/tb";
 import { BiArchiveIn} from "react-icons/bi";
 import "./note.css";
 
@@ -29,12 +29,24 @@ function Notes({id, text, archived, setNotes}){
         }
     }
 
+    const pinNote = async() => {
+        try {
+            const response = await api.put(`/notes/fixed/${id}`);
+            if (response.status === 200){
+                alert ("Nota fixada com sucesso")
+            }
+        } catch(error){
+            console.log(error)
+        }
+    }
+
     return(
         <div className={`note ${isArchived ? "archived" : ""}`}>
             <span>{text}</span>
             <footer className="note-footer">
                 <TbTrash className="trash-note" onClick={handleMoveToTrash}></TbTrash>
                 <BiArchiveIn className="archive-note" onClick={handleArchiveNote}></BiArchiveIn>
+                <TbPin className="pin-note" onClick={pinNote}></TbPin>
             </footer>
         </div>
     )
